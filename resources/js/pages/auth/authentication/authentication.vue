d2b31b958c474c8c01a9031eee19df174b206073
Vic Pavlovsky <pavlovskyi.va@gmail.com>
2019-10-06 00:33:25 +0300

    -add start auth block

--- resources/js/pages/auth/authentication/authentication.vue
+++ resources/js/pages/auth/authentication/authentication.vue
@@ -1,7 +1,24 @@
 <template>
-    <div class="authentication">
-        authentication
-    </div>
+    <section class="auth">
+        <div class="auth_logo">
+            <div class="Logo_title">{{ __("authentication", "Swift") }}</div>
+            <div class="Logo_function">{{ __("authentication", "Callback") }}</div>
+        </div>
+        <div class="auth_method">
+            <span class="method_login">{{ __("authentication", "Login") }}</span>
+            /
+            <span class="method_regostration">{{ __("authentication", "Registration") }}</span>
+        </div>
+        <div class="auth_data">
+            <input class="auth_data-input" :placeholder="__('authentication', 'Email or phone number')" name="login">
+            <input class="auth_data-input" :placeholder=" __('authentication', 'Password')" name="password">
+            <div class="button">{{ __("authentication", "Login") }}</div>
+            <div class="auth_data-forgot">{{ __("authentication", "Forgot your password?") }}</div>
+        </div>
+
+
+
+    </section>
 </template>
 
 <style lang='scss'>
