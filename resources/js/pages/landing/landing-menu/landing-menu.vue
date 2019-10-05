3975844364f6c015a524a38fc5f20c3a8d56c3f3
Vitaliy <animus2393@gmail.com>
2019-10-05 16:24:49 +0300

    - add landing-menu

--- resources/js/pages/landing/landing-menu/landing-menu.vue
+++ resources/js/pages/landing/landing-menu/landing-menu.vue
@@ -2,16 +2,20 @@
     <div class="landing-menu">
         <div class="landing-menu_container">
             <div class="landing-menu_logo">
-                <span>SWIFT</span>
-                <span>callback</span>
+                <span class="landing-menu_logoSwift">{{ __("landing", "Swift") }}</span>
+                <span class="landing-menu_logoCallback">{{ __("landing", "Callback") }}</span>
             </div>
 
             <div class="landing-menu_block">
-
+                <span class="landing-menu_block-item">{{ __("landing", "TARIF") }}</span>
+                <span class="landing-menu_block-item">{{ __("landing", "PRODUS") }}</span>
+                <span class="landing-menu_block-item">{{ __("landing", "DEZVOLTATORI") }}</span>
+                <span class="landing-menu_block-item">{{ __("landing", "SUPPORT") }}</span>
+                <span class="landing-menu_block-item">{{ __("landing", "CONTACTE") }}</span>
             </div>
 
             <div class="button mod-blue">
-                <span>Autentificare</span>
+                <span class="button_text">{{ __("landing", "button-text") }}</span>
             </div>
         </div>
     </div>
