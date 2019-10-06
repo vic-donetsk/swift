eb1ac6eb078f74f3d8ca05da8d1ac98666b5101a
Vic Pavlovsky <pavlovskyi.va@gmail.com>
2019-10-06 22:55:20 +0300

    #auth @layout -html done

--- resources/js/pages/auth/authentication/authentication.js
+++ resources/js/pages/auth/authentication/authentication.js
@@ -5,18 +5,27 @@ export default {
             // 0 - log in form,
             // 1 - 1st registration form
             // 2 - 2nd registration form
+            // 3 - password restore form
+            headerClass : "authHeader"
 
         }
     },
     methods: {
         gotoAuth() {
             this.authMode = 0;
+            this.headerClass = "authHeader";
         },
         gotoReg() {
             this.authMode = 1;
+            this.headerClass = "regHeader";
         },
         continueReg() {
             this.authMode = 2;
+            this.headerClass = "regHeader";
+        },
+        gotoRestore() {
+            this.authMode = 3;
+            this.headerClass = "restoreHeader";
         }
     },
 }
--- resources/js/pages/auth/authentication/authentication.vue
+++ resources/js/pages/auth/authentication/authentication.vue
@@ -1,10 +1,10 @@
 <template>
     <section class="auth">
-        <div class="auth_logo">
-            <div class="Logo_title">{{ __("authentication", "Swift") }}</div>
-            <div class="Logo_function">{{ __("authentication", "Callback") }}</div>
+        <div class="auth_logo" :class="headerClass">
+            <div class="logo_title">{{ __("authentication", "Swift") }}</div>
+            <div class="logo_function">{{ __("authentication", "Callback") }}</div>
         </div>
-        <div class="auth_method">
+        <div class="auth_method" v-if="authMode !== 3">
             <span class="method_login" @click="gotoAuth()">{{ __("authentication", "Login") }}</span>
             /
             <span class="method_registration" @click="gotoReg()">{{ __("authentication", "Registration") }}</span>
@@ -13,37 +13,39 @@
             <input class="auth-input" :placeholder="__('authentication', 'Email or phone number')" id="login">
             <input class="auth-input" :placeholder=" __('authentication', 'Password')" id="password">
             <div class="button mod-blue">{{ __("authentication", "Login") }}</div>
-            <div class="auth-passwordForgot">{{ __("authentication", "Forgot your password?") }}</div>
+            <div class="auth-passwordForgot" @click="gotoRestore()">{{ __("authentication", "Forgot your password?") }}</div>
         </div>
 
-        <div class="auth_regMode" v-else-if="authMode === 1">
+        <div class="auth_regMode" v-if="authMode === 1">
             <div class="regMode_step-active">1</div>
             <div class="regMode_step-passive">2</div>
-            <input class="auth-input" :placeholder="Email" id="email">
+            <input class="auth-input" placeholder="Email" id="email">
             <input class="auth-input" :placeholder="__('authentication', 'Password')" name="newPassword">
             <input class="auth-input" :placeholder="__('authentication', 'Phone number')" name="newPhone">
-            <div class="button mod-blue" @click="continueReg()">{{ __("authentication", "Create an account") }}</div>
-            <div class="regMode-text">{{ __("authentication", "By submitting this form, you accept") }}</div>
-            <span class="regMode-link">{{ __("authentication", "Terms of use") }}</span>
-            <span class="regMode-text">{{ __("authentication", "and") }}</span>
-            <span class="regMode-link">{{ __("authentication", "Privacy policy") }}</span>
+            <div class="button mod-blue" @click="continueReg()">{{ __("authentication", "Create an account") }}
+            </div>
         </div>
-
-        <div class="auth_regMode" v-else>
+        <div class="auth_regMode" v-if="authMode === 2">
             <div class="regMode_step-passive">1</div>
             <div class="regMode_step-active">2</div>
-            <input class="auth-input" :placeholder="__('authentication', 'First name')" id="email">
-            <input class="auth-input" :placeholder="__('authentication', 'Last name')" name="newPassword">
-            <input class="auth-input" :placeholder="__('authentication', 'Your site')" name="newPhone">
+            <input class="auth-input" :placeholder="__('authentication', 'First name')" id="firstNme">
+            <input class="auth-input" :placeholder="__('authentication', 'Last name')" name="lastName">
+            <input class="auth-input" :placeholder="__('authentication', 'Your site')" name="site">
             <div class="button mod-blue">{{ __("authentication", "Log In") }}</div>
-            <div class="regMode-text">{{ __("authentication", "By submitting this form, you accept") }}</div>
-            <span class="regMode-link">{{ __("authentication", "Terms of use") }}</span>
-            <span class="regMode-text">{{ __("authentication", "and") }}</span>
-            <span class="regMode-link">{{ __("authentication", "Privacy policy") }}</span>
         </div>
 
-
-
+        <div class="auth_method" v-if="authMode === 3">
+            <span class="method_restore">{{ __("authentication", "Password recovery") }}</span>
+        </div>
+        <div class="auth_restoreMode" v-if="authMode === 3">
+            <div class="restoreMode_text">{{ __("authentication", "Enter your email for recover your password.") }}</div>
+            <input class="auth-input" placeholder="Email" id="restoreEmail">
+            <div class="button mod-blue">{{ __("authentication", "Continue") }}</div>
+            <div class="restoreMode_back">
+                <span class="leftArrow"></span>
+                <span class="restoreMode_back-button" @click="gotoAuth()">{{ __("authentication", "Back") }}</span>
+            </div>
+        </div>
 
 
     </section>
