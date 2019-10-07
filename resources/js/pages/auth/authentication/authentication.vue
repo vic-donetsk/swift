9a5c57a19c502acf37bdced11b3030599c3a4f8b
Vic Pavlovsky <pavlovskyi.va@gmail.com>
2019-10-07 16:16:54 +0300

    -add styles to auth

--- resources/js/pages/auth/authentication/authentication.vue
+++ resources/js/pages/auth/authentication/authentication.vue
@@ -10,28 +10,46 @@
             <div class="auth_authMode" v-if="authMode === 0">
                 <input class="auth-input" :placeholder="__('authentication', 'Email or phone number')" id="login">
                 <input class="auth-input" :placeholder=" __('authentication', 'Password')" id="password">
-                <div class="button mod-blue">{{ __("authentication", "Login") }}</div>
+                <div class="auth_button button mod-blue">{{ __("authentication", "Login") }}</div>
                 <div class="auth-passwordForgot" @click="gotoRestore()">{{ __("authentication", "Forgot your password?")
                     }}
                 </div>
             </div>
 
             <div class="auth_regMode" v-if="authMode === 1">
-                <div class="regMode_step-active">1</div>
-                <div class="regMode_step-passive">2</div>
+                <div class="regMode_steps">
+                    <div class="regMode_step mod_active">1</div>
+                    <div class="regMode_step">2</div>
+                </div>
                 <input class="auth-input" placeholder="Email" id="email">
                 <input class="auth-input" :placeholder="__('authentication', 'Password')" name="newPassword">
                 <input class="auth-input" :placeholder="__('authentication', 'Phone number')" name="newPhone">
                 <div class="button mod-blue" @click="continueReg()">{{ __("authentication", "Create an account") }}
                 </div>
+
+                <div class="regMode-agree">{{ __("authentication", "By submitting this form, you accept") }}
+                    <a href="#" target="_blank" class="regMode-link">{{ __("authentication", "Terms of use") }}</a>&nbsp;
+                    {{ __("authentication", "and") }}&nbsp;
+                    <a href="#" target="_blank" class="regMode-link">{{ __("authentication", "Privacy policy") }}</a>
+                </div>
+
             </div>
             <div class="auth_regMode" v-if="authMode === 2">
-                <div class="regMode_step-passive">1</div>
-                <div class="regMode_step-active">2</div>
+                <div class="regMode_steps">
+                    <div class="regMode_step">1</div>
+                    <div class="regMode_step mod_active">2</div>
+                </div>
                 <input class="auth-input" :placeholder="__('authentication', 'First name')" id="firstNme">
                 <input class="auth-input" :placeholder="__('authentication', 'Last name')" name="lastName">
                 <input class="auth-input" :placeholder="__('authentication', 'Your site')" name="site">
                 <div class="button mod-blue">{{ __("authentication", "Log In") }}</div>
+
+                <div class="regMode-agree">{{ __("authentication", "By submitting this form, you accept") }}
+                    <a href="#" target="_blank" class="regMode-link">{{ __("authentication", "Terms of use") }}</a>&nbsp;
+                    {{ __("authentication", "and") }}&nbsp;
+                    <a href="#" target="_blank" class="regMode-link">{{ __("authentication", "Privacy policy") }}</a>
+                </div>
+
             </div>
 
             <div class="auth_method" v-if="authMode === 3">
