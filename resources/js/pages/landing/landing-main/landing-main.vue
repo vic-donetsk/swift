456f1f1607a320fb8d94a156af116ebd6760a81f
Vitaliy <animus2393@gmail.com>
2019-10-08 18:27:34 +0300

    - add landing-main content

--- resources/js/pages/landing/landing-main/landing-main.vue
+++ resources/js/pages/landing/landing-main/landing-main.vue
@@ -1,19 +1,20 @@
 <template>
     <div class="landing-main">
-        <div class="landing-main_container">
-            <div class="landing-main_numbering">
 
-            </div>
+        <div class="landing-main_numbering">
+            <span class="landing-main_number">1</span>
+        </div>
+        <div class="landing-main_content">
             <div class="landing-main_browser">
                 <div class="landing-browser_head">
                     <div class="browser-head_buttons">
-                        <div class="browser-head-item red"></div>
-                        <div class="browser-head-item yellow"></div>
-                        <div class="browser-head-item green"></div>
+                        <div class="browser-buttons-item red"></div>
+                        <div class="browser-buttons-item yellow"></div>
+                        <div class="browser-buttons-item green"></div>
                     </div>
                     <div class="browser-head_buttons">
-                        <div class="browser-head-elem"></div>
-                        <div class="browser-head-elem"></div>
+                        <div class="browser-buttons-elem"></div>
+                        <div class="browser-buttons-elem"></div>
                     </div>
 
                     <div class="browser-head_search">
@@ -21,9 +22,9 @@
                     </div>
 
                     <div class="browser-head_buttons">
-                        <div class="browser-head-elem"></div>
-                        <div class="browser-head-elem"></div>
-                        <div class="browser-head-elem"></div>
+                        <div class="browser-buttons-elem"></div>
+                        <div class="browser-buttons-elem"></div>
+                        <div class="browser-buttons-elem"></div>
                     </div>
                 </div>
 
@@ -51,11 +52,70 @@
                 </div>
             </div>
 
-            <div class="landing-main-message">
-
+            <div class="landing-main_message">
+                <p class="main-message_text">{{ __("landing", "Landing main message one") }}</p>
             </div>
+
         </div>
 
+        <div class="landing-main_numbering mod_margin">
+            <span class="landing-main_number">2</span>
+        </div>
+
+        <div class="landing-main_content">
+            <div class="landing-main_message mod-message_size">
+                <p class="main-message_text">{{ __("landing", "Landing main message two") }}</p>
+            </div>
+            <div class="landing-main_browser">
+                <div class="landing-browser_head">
+                    <div class="browser-head_buttons">
+                        <div class="browser-buttons-item red"></div>
+                        <div class="browser-buttons-item yellow"></div>
+                        <div class="browser-buttons-item green"></div>
+                    </div>
+                    <div class="browser-head_buttons">
+                        <div class="browser-buttons-elem"></div>
+                        <div class="browser-buttons-elem"></div>
+                    </div>
+
+                    <div class="browser-head_search">
+                        <span>{{ __("landing", "Browser search placeholder") }}</span>
+                    </div>
+
+                    <div class="browser-head_buttons">
+                        <div class="browser-buttons-elem"></div>
+                        <div class="browser-buttons-elem"></div>
+                        <div class="browser-buttons-elem"></div>
+                    </div>
+                </div>
+
+                <div class="landing-browser_content">
+                    <div class="browser-content_block">
+                        <div class="browser-content-item"></div>
+                        <div class="browser-content-item"></div>
+                        <div class="browser-content-item mod-size_left"></div>
+                    </div>
+                    <div class="browser-content_block">
+                        <div class="browser-content-item mod-size_right"></div>
+                        <div class="browser-content_elems">
+                            <div class="browser-content-item mod-size_small"></div>
+                            <div class="browser-content-item mod-size_small"></div>
+                            <div class="browser-content-item mod-size_small"></div>
+                            <div class="browser-content-item mod-size_small"></div>
+                            <div class="browser-content-item mod-size_small"></div>
+                            <div class="browser-content-item mod-size_small"></div>
+                            <div class="browser-content-item mod-size_small"></div>
+                            <div class="browser-content-item mod-size_small"></div>
+                            <div class="browser-content-item mod-size_small"></div>
+                            <div class="browser-content-item mod-size_small"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
     </div>
 </template>
 
