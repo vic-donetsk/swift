d3db3d94d26016ddfa2a4320f95fcb2c48fde57a
Vitaliy <animus2393@gmail.com>
2019-10-10 23:24:39 +0300

    - fix landing-offers svg

--- resources/js/pages/landing/landing-offers/landing-offers.js
+++ resources/js/pages/landing/landing-offers/landing-offers.js
@@ -4,11 +4,35 @@ export default {
             offers: [
                 {
                     title: this.__('landing', 'Offer 1'),
-                    icon: 'user'
+                    icon: 'dashnoard'
                 },
                 {
                     title: this.__('landing', 'Offer 2'),
-                    icon: 'user'
+                    icon: 'call-recorder'
+                },
+                {
+                    title: this.__('landing', 'Offer 2'),
+                    icon: 'call-schedulling'
+                },
+                {
+                    title: this.__('landing', 'Offer 2'),
+                    icon: 'sms-manager'
+                },
+                {
+                    title: this.__('landing', 'Offer 2'),
+                    icon: 'social-media'
+                },
+                {
+                    title: this.__('landing', 'Offer 2'),
+                    icon: 'api'
+                },
+                {
+                    title: this.__('landing', 'Offer 2'),
+                    icon: 'phone-book'
+                },
+                {
+                    title: this.__('landing', 'Offer 2'),
+                    icon: 'safe'
                 },
             ]
         }
