aea7234faa0e1820945e395f039b25bb92b7dc8b
onevanivan <onevanivan1@gmail.com>
2019-10-16 00:29:23 +0300

    fix landing-plan swiper

--- resources/js/pages/landing/landing-plan/landing-plan.js
+++ resources/js/pages/landing/landing-plan/landing-plan.js
@@ -1,31 +1,44 @@
 export default {
+    data() {
+        return {
+            initSwiper: false
+        }
+    },
     mounted() {
-        if ($(window).width() <= 1280) {
-            let landingPlanSwiper = new Swiper('.landing-plan .swiper-container', {
+        let landingPlanSwiper;
+
+        function init() {
+            landingPlanSwiper = new Swiper('.landing-plan .swiper-container', {
                 slidesPerView: 1,
                 loop: true,
                 paginationClickable: true,
                 speed: 300,
                 grabCursor: true,
-                // autoplay: {
-                //     delay: 7000,
-                //     disableOnInteraction: false
-                // },
                 navigation: {
                     nextEl: '.landing-plan .swiper-button-next',
                     prevEl: '.landing-plan .swiper-button-prev',
                 },
-
                 pagination: {
                     el: '.landing-plan .swiper-pagination',
                     clickable: true,
                 }
-
-            });
-
-            $(window).on('resize', function () {
-                landingPlanSwiper.update();
             });
         }
+
+        $(window).on('resize load', function () {
+            if ($(window).width() <= 1280) {
+                if (!this.initSwiper) {
+                    init();
+                    this.initSwiper = true;
+                } else {
+                    landingPlanSwiper.update();
+                }
+            } else {
+                if (this.initSwiper) {
+                    landingPlanSwiper.destroy();
+                    this.initSwiper = false;
+                }
+            }
+        });
     }
 }
