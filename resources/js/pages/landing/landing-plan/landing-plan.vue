5614c0a9469759f62a1429f3de23e3bc80382d58
Vitaliy <animus2393@gmail.com>
2019-10-15 19:30:12 +0300

    - fix landing-plan name

--- resources/js/pages/landing/landing-plan/landing-plan.vue
+++ resources/js/pages/landing/landing-plan/landing-plan.vue
@@ -6,13 +6,16 @@
         <div class="landing-plan_container">
             <div class="landing-plan_block left">
                 <img src="/img/illustration/Starter.svg" class="landing-plan_image">
+                <h3 class="landing-plan_name">STARTER</h3>
             </div>
 
             <div class="landing-plan_block middle">
                 <img src="/img/illustration/Profesional.svg" class="landing-plan_image">
+                <h3 class="landing-plan_name">PROFESIONAL</h3>
             </div>
             <div class="landing-plan_block right">
                 <img src="/img/illustration/Premium.svg" class="landing-plan_image">
+                <h3 class="landing-plan_name">PREMIUM</h3>
             </div>
         </div>
 
