c71d62d52e5641ec64efc221ffe8989a3dd9c2e9
Vitaliy <animus2393@gmail.com>
2019-10-15 23:02:42 +0300

    - fix landing-plan swiper

--- resources/js/pages/landing/landing-plan/landing-plan.js
+++ resources/js/pages/landing/landing-plan/landing-plan.js
@@ -1,7 +1,31 @@
 export default {
     mounted() {
         if ($(window).width() <= 1280) {
-            // Сюда пилить свайпер
+            let landingPlanSwiper = new Swiper('.landing-plan .swiper-container', {
+                slidesPerView: 1,
+                loop: true,
+                paginationClickable: true,
+                speed: 300,
+                grabCursor: true,
+                // autoplay: {
+                //     delay: 7000,
+                //     disableOnInteraction: false
+                // },
+                navigation: {
+                    nextEl: '.landing-plan .swiper-button-next',
+                    prevEl: '.landing-plan .swiper-button-prev',
+                },
+
+                pagination: {
+                    el: '.landing-plan .swiper-pagination',
+                    clickable: true,
+                }
+
+            });
+
+            $(window).on('resize', function () {
+                landingPlanSwiper.update();
+            });
         }
     }
 }
--- resources/js/pages/landing/landing-plan/landing-plan.vue
+++ resources/js/pages/landing/landing-plan/landing-plan.vue
@@ -4,69 +4,85 @@
             <span>{{__("landing", "Plan title")}}</span>
         </h2>
         <div class="landing-plan_container">
-            <div class="landing-plan_block left">
-                <img src="/img/illustration/Starter.svg" class="landing-plan_image">
-                <h3 class="landing-plan_name">STARTER</h3>
-                <div class="landing-plan_price">
-                    <div class="landin-plan_sum">
-                        <svg class="landing-plan_sum-icon">
-                            <use xlink:href="#euro"></use>
-                        </svg>
-                        <span class="landing-plan_sum-number">15</span>
+            <div class="swiper-container">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <div class="landing-plan_block left">
+                            <img src="/img/illustration/Starter.svg" class="landing-plan_image">
+                            <h3 class="landing-plan_name">STARTER</h3>
+                            <div class="landing-plan_price">
+                                <div class="landin-plan_sum">
+                                    <svg class="landing-plan_sum-icon">
+                                        <use xlink:href="#euro"></use>
+                                    </svg>
+                                    <span class="landing-plan_sum-number">15</span>
+                                </div>
+                                <span class="landing-plan_price-text">{{__("landing", "Price text")}}</span>
+                            </div>
+
+                            <div class="button mod_landing mod-blue">
+                                <span class="button_text">{{ __("landing", "Plan button") }}</span>
+                            </div>
+
+                            <h4 class="landing-plan_list-title">{{__("landing", "Plan list title")}}</h4>
+                            <ul class="landing-plan_list">
+                                <li>{{__("landing", "Plan list left 1")}}</li>
+                                <li>{{__("landing", "Plan list left 2")}}</li>
+                                <li>{{__("landing", "Plan list left 3")}}</li>
+                            </ul>
+                        </div>
                     </div>
-                    <span class="landing-plan_price-text">{{__("landing", "Price text")}}</span>
+
+                    <div class="swiper-slide">
+                        <div class="landing-plan_block middle">
+                            <img src="/img/illustration/Profesional.svg" class="landing-plan_image">
+                            <h3 class="landing-plan_name">PROFESIONAL</h3>
+                            <div class="landing-plan_price">
+                                <div class="landin-plan_sum">
+                                    <svg class="landing-plan_sum-icon">
+                                        <use xlink:href="#euro"></use>
+                                    </svg>
+                                    <span class="landing-plan_sum-number">35</span>
+                                </div>
+                                <span class="landing-plan_price-text">{{__("landing", "Price text")}}</span>
+                            </div>
+
+                            <div class="button mod_landing mod-green">
+                                <span class="button_text">{{ __("landing", "Plan button") }}</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="swiper-slide">
+                        <div class="landing-plan_block right">
+                            <img src="/img/illustration/Premium.svg" class="landing-plan_image">
+                            <h3 class="landing-plan_name">PREMIUM</h3>
+                            <div class="landing-plan_price">
+                                <div class="landin-plan_sum">
+                                    <svg class="landing-plan_sum-icon">
+                                        <use xlink:href="#euro"></use>
+                                    </svg>
+                                    <span class="landing-plan_sum-number">55</span>
+                                </div>
+                                <span class="landing-plan_price-text">{{__("landing", "Price text")}}</span>
+                            </div>
+
+                            <div class="button mod_landing mod-brown">
+                                <span class="button_text">{{ __("landing", "Plan button") }}</span>
+                            </div>
+
+                            <div class="landing-plan_list">
+
+                            </div>
+                        </div>
+                    </div>
+
+
+
                 </div>
 
-                <div class="button mod_landing mod-blue">
-                    <span class="button_text">{{ __("landing", "Plan button") }}</span>
-                </div>
-
-                <h4 class="landing-plan_list-title">{{__("landing", "Plan list title")}}</h4>
-                <ul class="landing-plan_list">
-                        <li>{{__("landing", "Plan list left 1")}}</li>
-                        <li>{{__("landing", "Plan list left 2")}}</li>
-                        <li>{{__("landing", "Plan list left 3")}}</li>
-                </ul>
             </div>
 
-            <div class="landing-plan_block middle">
-                <img src="/img/illustration/Profesional.svg" class="landing-plan_image">
-                <h3 class="landing-plan_name">PROFESIONAL</h3>
-                <div class="landing-plan_price">
-                    <div class="landin-plan_sum">
-                        <svg class="landing-plan_sum-icon">
-                            <use xlink:href="#euro"></use>
-                        </svg>
-                        <span class="landing-plan_sum-number">35</span>
-                    </div>
-                    <span class="landing-plan_price-text">{{__("landing", "Price text")}}</span>
-                </div>
-
-                <div class="button mod_landing mod-green">
-                    <span class="button_text">{{ __("landing", "Plan button") }}</span>
-                </div>
-            </div>
-            <div class="landing-plan_block right">
-                <img src="/img/illustration/Premium.svg" class="landing-plan_image">
-                <h3 class="landing-plan_name">PREMIUM</h3>
-                <div class="landing-plan_price">
-                    <div class="landin-plan_sum">
-                        <svg class="landing-plan_sum-icon">
-                            <use xlink:href="#euro"></use>
-                        </svg>
-                        <span class="landing-plan_sum-number">55</span>
-                    </div>
-                    <span class="landing-plan_price-text">{{__("landing", "Price text")}}</span>
-                </div>
-
-                <div class="button mod_landing mod-brown">
-                    <span class="button_text">{{ __("landing", "Plan button") }}</span>
-                </div>
-
-                <div class="landing-plan_list">
-
-                </div>
-            </div>
         </div>
 
     </div>
