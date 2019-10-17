29f1115f27968e5ae13bac7d705fa41538e07222
Vitaliy <animus2393@gmail.com>
2019-10-17 18:54:57 +0300

    - fix sidebar person

--- resources/js/pages/cabinet/components/cabinet-sidebar/cabinet-sidebar.vue
+++ resources/js/pages/cabinet/components/cabinet-sidebar/cabinet-sidebar.vue
@@ -1,7 +1,17 @@
 <template>
     <aside class="cabinet-sidebar">
         <div class="cabinet-sidebar_content">
+            <logo size="landing"></logo>
 
+            <div class="cabinet-sidebar_person">
+                <img src="/img/person.png" class="sidebar-person_image">
+
+                <div class="sidebar-person_name">
+                    <span>  {{__('widget','Cabinet sidebar name')}}</span>
+
+                    <svg class="sidebar-person_icon"><use xlink:href="#arrow"></use></svg>
+                </div>
+            </div>
         </div>
     </aside>
 </template>
