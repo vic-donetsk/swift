6a010ef1e370ba30bef2716dd051667bfc119a54
Vitaliy <animus2393@gmail.com>
2019-10-17 19:27:16 +0300

    - fix sidebar menu

--- resources/js/pages/cabinet/components/cabinet-sidebar/cabinet-sidebar.vue
+++ resources/js/pages/cabinet/components/cabinet-sidebar/cabinet-sidebar.vue
@@ -7,9 +7,41 @@
                 <img src="/img/person.png" class="sidebar-person_image">
 
                 <div class="sidebar-person_name">
-                    <span>  {{__('widget','Cabinet sidebar name')}}</span>
+                    <span>{{__('widget','Cabinet sidebar name')}}</span>
 
                     <svg class="sidebar-person_icon"><use xlink:href="#arrow"></use></svg>
+                </div>
+            </div>
+
+            <div class="cabinet-sidebar_menu">
+                <div class="cabinet-sidebar_item">
+                    <svg class="sidebar-menu_icon">
+                        <use xlink:href="#statistics"></use>
+                    </svg>
+                    <span class="sidebar-menu_name">{{__('widget','Sidebar menu name 1')}}</span>
+                </div>
+
+                <div class="cabinet-sidebar_item">
+                    <svg class="sidebar-menu_icon">
+                        <use xlink:href="#widget"></use>
+                    </svg>
+                    <span class="sidebar-menu_name">{{__('widget','Sidebar menu name 2')}}</span>
+                </div>
+
+                <div class="cabinet-sidebar_item">
+
+                </div>
+
+                <div class="cabinet-sidebar_item">
+
+                </div>
+
+                <div class="cabinet-sidebar_item">
+
+                </div>
+
+                <div class="cabinet-sidebar_item">
+
                 </div>
             </div>
         </div>
