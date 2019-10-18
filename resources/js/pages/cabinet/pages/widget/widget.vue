93de03f4d0be61aad855f9db6604d623c3c5088e
Vitaliy <animus2393@gmail.com>
2019-10-18 22:45:53 +0300

    - fix cabinet page elements & icons

--- resources/js/pages/cabinet/pages/widget/widget.vue
+++ resources/js/pages/cabinet/pages/widget/widget.vue
@@ -21,36 +21,81 @@
 
                     <div class="widget-block_item">
                         <div class="widget-block_elem-wrap">
-                            <div class="widget-block_elem mod_border"></div>
-                            <div class="widget-block_elem"></div>
+                            <div class="widget-block_elem left mod_color">
+                                <svg class="widget-elem_icon">
+                                    <use xlink:href="#phone-widget"></use>
+                                </svg>
+                                <span class="widget_elem_text">{{__('widget','Widget elem text 1')}}</span>
+                            </div>
+
+                            <div class="widget-block_elem right">
+                                <span>{{__('widget','Widget contact phone 1')}}</span>
+                                <span>{{__('widget','Widget contact phone 2')}}</span>
+                            </div>
                         </div>
 
                         <div class="widget-block_elem-wrap">
-                            <div class="widget-block_elem mod_border"></div>
-                            <div class="widget-block_elem"></div>
+                            <div class="widget-block_elem left">
+                                <svg class="widget-elem_icon">
+                                    <use xlink:href="#wall-clock"></use>
+                                </svg>
+                                <span class="widget_elem_text">{{__('widget','Widget elem text 2')}}</span>
+                            </div>
+                            <div class="widget-block_elem right">
+                                <span>{{__('widget','Widget elem text 3')}}</span>
+                                <span>{{__('widget','Widget elem text 4')}}</span>
+                            </div>
                         </div>
 
                         <div class="widget-block_elem-wrap">
-                            <div class="widget-block_elem mod_border"></div>
-                            <div class="widget-block_elem"></div>
+                            <div class="widget-block_elem left">
+                                <svg class="widget-elem_icon">
+                                    <use xlink:href="#cellphone-sms"></use>
+                                </svg>
+                                <span class="widget_elem_text">{{__('widget','Widget elem text 5')}}</span>
+                            </div>
+                            <div class="widget-block_elem right">
+                                <span>{{__('widget','Widget contact phone 1')}}</span>
+                                <span>{{__('widget','Widget contact phone 2')}}</span>
+                            </div>
                         </div>
-
                     </div>
 
                     <div class="widget-block_item">
                         <div class="widget-block_elem-wrap">
-                            <div class="widget-block_elem mod_border"></div>
-                            <div class="widget-block_elem"></div>
+                            <div class="widget-block_elem left">
+                                <svg class="widget-elem_icon">
+                                    <use xlink:href="#chat-bubbles"></use>
+                                </svg>
+                                <span class="widget_elem_text">{{__('widget','Widget elem text 6')}}</span>
+                            </div>
+                            <div class="widget-block_elem right">
+                                <span>{{__('widget','Widget elem text 7')}}</span>
+                            </div>
                         </div>
 
                         <div class="widget-block_elem-wrap">
-                            <div class="widget-block_elem mod_border"></div>
-                            <div class="widget-block_elem"></div>
+                            <div class="widget-block_elem left">
+                                <svg class="widget-elem_icon">
+                                    <use xlink:href="#mail-envelope"></use>
+                                </svg>
+                                <span class="widget_elem_text">{{__('widget','Widget elem text 8')}}</span>
+                            </div>
+                            <div class="widget-block_elem right">
+                                <span>{{__('widget','Widget elem text 9')}}</span>
+                            </div>
                         </div>
 
                         <div class="widget-block_elem-wrap">
-                            <div class="widget-block_elem mod_border"></div>
-                            <div class="widget-block_elem"></div>
+                            <div class="widget-block_elem left">
+                                <svg class="widget-elem_icon">
+                                    <use xlink:href="#user"></use>
+                                </svg>
+                                <span class="widget_elem_text">{{__('widget','Widget elem text 10')}}</span>
+                            </div>
+                            <div class="widget-block_elem right">
+                                <span>{{__('widget','Widget elem text 11')}}</span>
+                            </div>
                         </div>
 
                     </div>
