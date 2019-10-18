cca6acbba61b866cb609a153b63835a38e8d921d
Vitaliy <animus2393@gmail.com>
2019-10-18 16:54:33 +0300

    - fix cabinet page

--- resources/js/pages/cabinet/pages/widget/widget.vue
+++ resources/js/pages/cabinet/pages/widget/widget.vue
@@ -6,7 +6,31 @@
         <cabinet-sidebar></cabinet-sidebar>
         <div class="cabinet_page">
             <div class="widget_container">
+                <h2 class="cabinet-page_title">
+                    <svg class="cabinet-page_title-icon">
+                        <use xlink:href="#global"></use>
+                    </svg>
+                    <span>{{__('widget','Cabinet page title')}}</span>
+                </h2>
 
+                <div class="widget-block">
+                    <div class="widget-block_item">
+                        <div class="widget-block_elem mod_size"></div>
+                        <div class="widget-block_btn mod_play"></div>
+                    </div>
+
+                    <div class="widget-block_item">
+                        <div class="widget-block_elem"></div>
+                        <div class="widget-block_elem"></div>
+                        <div class="widget-block_elem"></div>
+                    </div>
+
+                    <div class="widget-block_item">
+                        <div class="widget-block_elem"></div>
+                        <div class="widget-block_elem"></div>
+                        <div class="widget-block_elem"></div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
