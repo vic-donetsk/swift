2a3ceccf82730ef33a21c38925526464cfc0fe08
Vitaliy <animus2393@gmail.com>
2019-10-19 20:18:33 +0300

    - fix widget block, buttons

--- resources/views/sprite.blade.php
+++ resources/views/sprite.blade.php
@@ -391,7 +391,7 @@
         </svg>
     </symbol>
 
-    <symbol id="chat-bubbles" >
+    <symbol id="chat-bubbles">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 34.996 27.62">
             <g transform="translate(0 -3.241)">
                 <path d="M31.33,9.67h-.958V20.205a2.852,2.852,0,0,1-2.87,3.124H8.743v.5a3.379,3.379,0,0,0,3.343,2.914H26.428l5.487,3.226-.8-3.226h.211c1.693,0,2.457-1.46,2.457-2.914V12.171A2.258,2.258,0,0,0,31.33,9.67Z" transform="translate(1.21 0.89)"/>
@@ -401,7 +401,11 @@
     </symbol>
 
     <symbol id="pause-solid">
-        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="pause" class="svg-inline--fa fa-pause fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M144 479H48c-26.5 0-48-21.5-48-48V79c0-26.5 21.5-48 48-48h96c26.5 0 48 21.5 48 48v352c0 26.5-21.5 48-48 48zm304-48V79c0-26.5-21.5-48-48-48h-96c-26.5 0-48 21.5-48 48v352c0 26.5 21.5 48 48 48h96c26.5 0 48-21.5 48-48z"></path></svg>
+        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="pause" class="svg-inline--fa fa-pause fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M144 479H48c-26.5 0-48-21.5-48-48V79c0-26.5 21.5-48 48-48h96c26.5 0 48 21.5 48 48v352c0 26.5-21.5 48-48 48zm304-48V79c0-26.5-21.5-48-48-48h-96c-26.5 0-48 21.5-48 48v352c0 26.5 21.5 48 48 48h96c26.5 0 48-21.5 48-48z"></path></svg>
+    </symbol>
+
+    <symbol id="play">
+        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="play" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z"></path></svg>
     </symbol>
 </svg>
 
