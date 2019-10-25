af578eeb39f78b31935eb7eb78125d18910207f8
Vic Pavlovsky <pavlovskyi.va@gmail.com>
2019-10-25 22:11:28 +0300

    #statistics -add charts block

--- resources/js/components/chart-js/chart-js.vue
+++ resources/js/components/chart-js/chart-js.vue
@@ -1,7 +1,19 @@
 <template>
-    <div >
+    <div>
         <div class="diagram_header">
+            <div class="diagram_header-title">
+                <div class="title_numbers">
+                    <div class="title_numbers-total">{{total}}</div>
+                    <div class="title_numbers-percent"
+                         :class="(isIncrease == 'true') ? 'mod_increase' : 'mod_decrease'">{{percentage}}%
+                    </div>
+                </div>
+                <div class="title_name">{{chartTitle}}</div>
 
+            </div>
+            <div class="diagram_header-avatar">
+                <img :src="avatar" alt="">
+            </div>
         </div>
         <div class="diagram_canvas-container">
             <canvas :id="chartId" class="diagram_chart">
