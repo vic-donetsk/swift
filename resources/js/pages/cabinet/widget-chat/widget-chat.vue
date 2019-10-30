70b19c6e3ab761c197e6d59105bf091b0a670660
Vitaliy <animus2393@gmail.com>
2019-10-30 22:37:43 +0300

    - fix widget chat table mobile

--- resources/js/pages/cabinet/widget-chat/widget-chat.vue
+++ resources/js/pages/cabinet/widget-chat/widget-chat.vue
@@ -152,6 +152,10 @@
 
                     <div class="widget-chat_table">
                         <div class="widget-chat_table-head">
+
+                            <div class="widget-table_head-item mod-add_btn">
+
+                            </div>
                             <div class="widget-table_head-item mod-name">
                                 <span>{{__('widget','Widget chat head 1')}}</span>
                             </div>
@@ -189,6 +193,11 @@
 
                         <div class="widget-chat_table-content">
                             <div class="widget-chat_table-string">
+
+                                <div class="widget-table_item mod-add_btn">
+
+                                </div>
+
                                 <div class="widget-table_item mod-name">
                                     <span>{{__('widget','Widget name 1')}}</span>
                                     <img src="/img/Adrian_Ionascu.png" class="widget-chat_image">
@@ -237,7 +246,11 @@
                                     </svg>
                                 </div>
                             </div>
+
                             <div class="widget-chat_table-string">
+                                <div class="widget-table_item mod-add_btn">
+
+                                </div>
                                 <div class="widget-table_item mod-name">
                                     <span class="widget-table-text">{{__('widget','Widget name 2')}}</span>
                                     <img src="/img/Mark_Antonov.png" class="widget-chat_image">
@@ -289,46 +302,47 @@
                         </div>
                     </div>
                 </div>
+            </div>
 
-                <div class="widget-chat_block mod-inputs">
-                    <h2 class="cabinet-page_title">
-                        <svg class="cabinet-page_title-icon">
-                            <use xlink:href="#user"></use>
-                        </svg>
-                        <span>{{__('widget','Cabinet page title 13')}}</span>
-                    </h2>
+            <div class="widget-chat_block mod-inputs">
+                <h2 class="cabinet-page_title">
+                    <svg class="cabinet-page_title-icon">
+                        <use xlink:href="#user"></use>
+                    </svg>
+                    <span>{{__('widget','Cabinet page title 13')}}</span>
+                </h2>
 
-                    <div class="widget-chat_inputs">
-                        <div class="widget-input_wrap">
-                            <span class="widget-input_title">{{__('widget','Widget input title 1')}}</span>
-                            <input type="text"
-                                   class="widget-input"
-                                   :placeholder='__("widget", "Widget input placeholder 2")'>
-                        </div>
-
-                        <div class="widget-input_wrap">
-                            <span class="widget-input_title">{{__('widget','Widget input title 2')}}</span>
-                            <input type="text"
-                                   class="widget-input"
-                                   :placeholder='__("widget", "Widget input placeholder 3")'>
-                        </div>
-
-
-                        <div class="widget-input_wrap">
-                            <span class="widget-input_title">{{__('widget','Widget input title 3')}}</span>
-                            <input type="text"
-                                   class="widget-input"
-                                   :placeholder='__("widget", "Widget contact phone 1")'>
-                        </div>
+                <div class="widget-chat_inputs">
+                    <div class="widget-input_wrap">
+                        <span class="widget-input_title">{{__('widget','Widget input title 1')}}</span>
+                        <input type="text"
+                               class="widget-input"
+                               :placeholder='__("widget", "Widget input placeholder 2")'>
                     </div>
 
-                    <div class="widget-button">
-                        <span>{{__('widget','Widget deactivate button 2')}}</span>
+                    <div class="widget-input_wrap">
+                        <span class="widget-input_title">{{__('widget','Widget input title 2')}}</span>
+                        <input type="text"
+                               class="widget-input"
+                               :placeholder='__("widget", "Widget input placeholder 3")'>
                     </div>
 
+
+                    <div class="widget-input_wrap">
+                        <span class="widget-input_title">{{__('widget','Widget input title 3')}}</span>
+                        <input type="text"
+                               class="widget-input"
+                               :placeholder='__("widget", "Widget contact phone 1")'>
+                    </div>
                 </div>
+
+                <div class="widget-button">
+                    <span>{{__('widget','Widget deactivate button 2')}}</span>
+                </div>
+
             </div>
         </div>
+    </div>
     </div>
 </template>
 
