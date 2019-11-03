70842c1d9f78e6d56404e866852721dedbad8b21
onevanivan <onevanivan1@gmail.com>
2019-11-03 14:52:52 +0200

    fix cabinet

--- resources/js/pages/landing/landing-header/landing-header.js
+++ resources/js/pages/landing/landing-header/landing-header.js
@@ -1,18 +1,31 @@
 import Logo from './../../../components/logo/logo.vue';
 
-
-
 export default {
     components: {
         'logo': Logo,
     },
     data: function () {
         return {
-
+            menu: false
         }
     },
     methods: {
+        openMenu() {
+            this.menu = !this.menu;
 
+            $('.landing-header .landing-mobile_menu').slideToggle(100);
+
+            if (this.menu) {
+                $('body').css({
+                    'overflow': 'hidden'
+                });
+            } else {
+                $('body').css({
+                    'overflow': 'visible'
+                });
+            }
+
+        }
     },
 }
 
