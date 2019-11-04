7549c64e22bca149e457a7c9734a31879981b3bd
Vitaliy <animus2393@gmail.com>
2019-11-04 13:49:23 +0200

    - fix billing bank inputs

--- resources/js/pages/cabinet/billing-bank/billing-bank.vue
+++ resources/js/pages/cabinet/billing-bank/billing-bank.vue
@@ -22,16 +22,48 @@
                     <span>{{__('billing','Billing menu text 3')}}</span>
                 </h2>
 
+                <div class="billing-inputs">
+                    <div class="widget-chat_inputs mod-left">
+                        <div class="widget-input_wrap">
+                            <span class="widget-input_title">{{__('billing','Billing input title 1')}}</span>
+                            <input type="text"
+                                   class="widget-input"
+                                   :placeholder='__("billing", "Billing input placeholder")'>
+                        </div>
+
+                        <div class="widget-input_wrap">
+                            <span class="widget-input_title">{{__('billing','Billing input title 2')}}</span>
+                            <input type="text"
+                                   class="widget-input"
+                                   :placeholder='__("billing", "Billing input placeholder")'>
+                        </div>
+
+
+                        <div class="widget-input_wrap">
+                            <span class="widget-input_title">{{__('billing','Billing input title 3')}}</span>
+                            <input type="text"
+                                   class="widget-input"
+                                   :placeholder='__("billing", "Billing input placeholder")'>
+                        </div>
+
+                        <div class="widget-input_wrap">
+                            <span class="widget-input_title">{{__('billing','Billing input title 4')}}</span>
+                            <input type="text"
+                                   class="widget-input"
+                                   :placeholder='__("billing", "Billing input placeholder")'>
+                        </div>
+                    </div>
+
                     <div class="widget-chat_inputs">
                         <div class="widget-input_wrap">
-                            <span class="widget-input_title">{{__('widget','Widget input title 1')}}</span>
+                            <span class="widget-input_title">{{__('billing','Billing input title 5')}}</span>
                             <input type="text"
                                    class="widget-input"
                                    :placeholder='__("billing", "Billing input placeholder")'>
                         </div>
 
                         <div class="widget-input_wrap">
-                            <span class="widget-input_title">{{__('widget','Widget input title 2')}}</span>
+                            <span class="widget-input_title">{{__('billing','Billing input title 6')}}</span>
                             <input type="text"
                                    class="widget-input"
                                    :placeholder='__("billing", "Billing input placeholder")'>
@@ -39,16 +71,18 @@
 
 
                         <div class="widget-input_wrap">
-                            <span class="widget-input_title">{{__('widget','Widget input title 3')}}</span>
+                            <span class="widget-input_title">{{__('billing','Billing input title 7')}}</span>
                             <input type="text"
                                    class="widget-input"
                                    :placeholder='__("billing", "Billing input placeholder")'>
                         </div>
+
+                        <div class="widget-button">
+                            <span>{{__('widget','Widget deactivate button 2')}}</span>
+                        </div>
                     </div>
 
-                    <div class="widget-button">
-                        <span>{{__('widget','Widget deactivate button 2')}}</span>
-                    </div>
+                </div>
 
 
             </div>
