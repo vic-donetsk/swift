0d58ec801c82c4c6bdf40c326f85abe2a033459d
Vic Pavlovsky <pavlovskyi.va@gmail.com>
2019-11-05 21:13:11 +0200

    #settings -add adaptive menu

--- resources/js/pages/cabinet/settings/settings.js
+++ resources/js/pages/cabinet/settings/settings.js
@@ -2,21 +2,43 @@ export default {
     data: function () {
         return {
             settingsMode: 0,
+            mobileMenuOpened: false,
             settingChapters: [
                 {title: 'general', svg: 'settings'},
                 {title: 'api keys', svg: 'api_gear'},
                 {title: 'blacklist', svg: 'cross'},
-                {title: 'session', svg: 'global'}]
+                {title: 'session', svg: 'global'}],
         }
     },
     created() {
+
     },
     mounted() {
     },
     methods: {
         changeMode(mode) {
-            if (this.settingMode !== mode)
+            if (this.settingsMode !== mode)
                 this.settingsMode = mode;
+        },
+        changeMobileMode(mode) {
+            let menuOptions = document.querySelectorAll('.mod_option');
+            if (this.settingsMode !== mode)
+                this.settingsMode = mode;
+            for (let oneOption of menuOptions) {
+                oneOption.classList.remove('mod_open');
+            }
+            this.mobileMenuOpened = !this.mobileMenuOpened;
+        },
+        showModes() {
+            let menuOptions = document.querySelectorAll('.mod_option');
+            for (let oneOption of menuOptions) {
+                if (this.mobileMenuOpened) {
+                    oneOption.classList.remove('mod_open');
+                } else {
+                    oneOption.classList.add('mod_open');
+                }
+            }
+            this.mobileMenuOpened = !this.mobileMenuOpened;
         }
 
     }
--- resources/js/pages/cabinet/settings/settings.vue
+++ resources/js/pages/cabinet/settings/settings.vue
@@ -1,7 +1,27 @@
 <template>
     <div class="cabinet_page settings">
+
         <nav class="settings_nav">
             <div v-for="(setting, index) in settingChapters" class="settings_nav-item" :class="{mod_active: settingsMode === index}" @click="changeMode(index)">
+                <svg class="nav-item_svg">
+                    <use :xlink:href="'#' + setting.svg"></use>
+                </svg>
+                <div class="nav-item_text">
+                    {{__('settings', setting.title)}}
+                </div>
+            </div>
+        </nav>
+
+        <nav class="settings_navMobile">
+            <div class="settings_nav-item mod_active mod_mobile mod_select" @click="showModes()">
+                <svg class="nav-item_svg">
+                    <use :xlink:href="'#' + settingChapters[settingsMode].svg"></use>
+                </svg>
+                <div class="nav-item_text">
+                    {{__('settings', settingChapters[settingsMode].title)}}
+                </div>
+            </div>
+            <div v-for="(setting, index) in settingChapters" class="settings_nav-item mod_mobile mod_option" @click="changeMobileMode(index)">
                 <svg class="nav-item_svg">
                     <use :xlink:href="'#' + setting.svg"></use>
                 </svg>
