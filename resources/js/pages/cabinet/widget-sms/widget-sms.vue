289c288493d6b53f65093266d228c0c285966f94
Vitaliy <animus2393@gmail.com>
2019-11-07 12:50:25 +0200

    - fix widget sms table

--- resources/js/pages/cabinet/widget-sms/widget-sms.vue
+++ resources/js/pages/cabinet/widget-sms/widget-sms.vue
@@ -77,68 +77,24 @@
 
                     <div class="widget-sms_table">
                         <div class="billing-invoice_table-head">
-
-                            <div class="billing-table_head-item mod-add_btn">
-
+                            <div class="billing-table_head-item mod-message">
+                                <span>{{__('widget','Widget schemes head 1')}}</span>
                             </div>
-                            <div class="billing-table_head-item mod-invoice">
-                                <span>{{__('billing','Billing invoice head 1')}}</span>
+                            <div class="billing-table_head-item mod-context mod-desktop">
+                                <span>{{__('widget','Widget schemes head 2')}}</span>
                             </div>
-                            <div class="billing-table_head-item mod-transaction mod-desktop">
-                                <span>{{__('billing','Billing invoice head 2')}}</span>
-                            </div>
-                            <div class="billing-table_head-item mod-price mod-desktop">
-                                <span>{{__('billing','Billing invoice head 3')}}</span>
+                            <div class="billing-table_head-item mod-description mod-desktop">
+                                <span>{{__('widget','Widget schemes head 3')}}</span>
                             </div>
 
-                            <div class="billing-table_head-item mod-download"></div>
+                            <div class="billing-table_head-item mod_switch">
+                                <span>{{__('widget','Widget schemes head 4')}}</span>
+                            </div>
 
                         </div>
 
                         <div class="billing-invoice_table-content">
-                            <div class="billing-invoice_table-row" v-for="item in 2">
-                                <div class="billing-invoice_table-row-cells">
-                                    <div class="billing-table_item mod-add_btn" @click="toggleRowList(item)">
-                                        <svg v-if="item !== rowList" class="mod-add_btn-icon">
-                                            <use xlink:href="#plus-add"></use>
-                                        </svg>
 
-                                        <svg v-else class="mod-add_btn-icon">
-                                            <use xlink:href="#minus"></use>
-                                        </svg>
-                                    </div>
-                                    <div class="billing-table_item mod-invoice">
-                                        <span>25152</span>
-                                    </div>
-                                    <div class="billing-table_item mod-transaction mod-desktop">
-                                        <span>25326453</span>
-                                    </div>
-                                    <div class="billing-table_item  mod-price mod-desktop">
-                                        <span>15 €</span>
-                                    </div>
-                                    <div class="billing-table_item mod-download">
-                                        <svg class="billing-table_icon-download">
-                                            <use xlink:href="#download"></use>
-                                        </svg>
-                                    </div>
-                                </div>
-
-
-                                <div class="billing-table_list" v-if="item === rowList">
-                                    <div class="billing-table_list-item">
-                                        <span class="billing-table_list-title">{{__('billing','Billing invoice head 2')}}</span>
-                                        <span class="billing-table_list-val"> : 25326453</span>
-                                    </div>
-                                    <div class="billing-table_list-item">
-                                        <span class="billing-table_list-title">{{__('billing','Billing invoice head 3')}}</span>
-                                        <span class="billing-table_list-val">: 15 €</span>
-                                    </div>
-                                    <div class="billing-table_list-item">
-                                        <span class="billing-table_list-title">{{__('billing','Billing invoice head 6')}}</span>
-                                        <span class="billing-table_list-val"> : {{__('billing','Billing invoice table content 7')}}</span>
-                                    </div>
-                                </div>
-                            </div>
                         </div>
                     </div>
 
