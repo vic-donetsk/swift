f9fabcf0114a363bf35c7e5a6065d0af59f48521
onevanivan <onevanivan1@gmail.com>
2019-11-14 19:03:56 +0200

    fix widget main

--- resources/js/pages/cabinet/widget/widget-main/widget-main.js
+++ resources/js/pages/cabinet/widget/widget-main/widget-main.js
@@ -1,18 +1,7 @@
+import widgetComponent from './widget-component/widget-component.vue';
+
 export default {
-    data: function () {
-        return {
-            widgetMainMenu:false,
-            widgetElemBottom1:false,
-            widgetElemBottom2:false,
-            widgetElemBottom3:false,
-            widgetElemBottom4:false,
-            widgetElemBottom5:false,
-            widgetElemBottom6:false,
-        }
-    },
-    methods: {
-        nextWidgetComponent() {
-            EventBus.$emit('widget-to-install');
-        }
+    components: {
+        'widget-component': widgetComponent
     }
 }
