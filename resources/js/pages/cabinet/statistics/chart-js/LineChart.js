732fc9c8f8e01888a5bb92ae4e57ad572e4f52ef
Vic Pavlovsky <pavlovskyi.va@gmail.com>
2019-11-16 20:55:22 +0200

    [pc] task 4 done

--- resources/js/pages/cabinet/statistics/chart-js/LineChart.js
+++ resources/js/pages/cabinet/statistics/chart-js/LineChart.js
@@ -6,7 +6,30 @@ export default {
     extends: Line,
     data() {
         return {
-            options: {
+            options: {},
+        }
+    },
+    mixins: [reactiveProp],
+    props: {
+        chartData: {
+            type: Object,
+            default: null
+        },
+        chartTitle: {
+            type: String,
+            default: ''
+        },
+    },
+    mounted() {
+        this.prepareOptions(this.chartTitle);
+        this.renderChart(this.chartData, this.options);
+    },
+    methods: {
+        prepareOptions(chartTitle) {
+
+            chartTitle = chartTitle.charAt(0).toUpperCase() + chartTitle.slice(1).toLowerCase();
+
+            this.options = {
 
                 legend: {
                     display: false, // turn off the title
@@ -40,43 +63,36 @@ export default {
                     titleFontSize: 12,
                     titleFontStyle: '400',
                     titleFontColor: '#BBC6D9',
+                    titleSpacing: 10,
                     bodyFontFamily: 'Axiforma',
                     bodyFontSize: 12,
                     bodyFontStyle: '300',
                     bodyAlign: 'right',
                     bodyFontColor: '#fff',
-                    xPadding: 15,
-                    yPadding: 15,
+                    bodySpacing: 10,
+                    xPadding: 17,
+                    yPadding: 16,
                     caretSize: 0,
                     cornerRadius: 9,
+                    borderColor: 'rgba(19, 34, 60, 0.21)',
+                    borderWidth: 5,
 
                     callbacks: {
-                        label: function(tooltipItem, data) {
+                        label: function (tooltipItem, data) {
                             var value = data.datasets[tooltipItem.datasetIndex].value || '';
 
-                            return this.chartTitle + tooltipItem.value;
+                            return chartTitle + '   ' + tooltipItem.value;
                         }
                     },
                 }
-            },
+            }
         }
     },
-    mixins: [reactiveProp],
-    props: {
-        chartData: {
-            type: Object,
-            default: null
-        },
-        chartTitle: {
-            type: String,
-            default: ''
-        },
-    },
-    mounted() {
-        this.renderChart(this.chartData, this.options);
-    },
+
     watch: {
-        chartData () {
+
+        chartData() {
+            this.prepareOptions(this.chartTitle);
             this.renderChart(this.chartData, this.options);
         }
     }
